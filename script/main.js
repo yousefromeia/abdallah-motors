d026223fa2ce7914342140646e2bd4a0eb6d0c80
yousefromeia <112512238+yousefromeia@users.noreply.github.com>
2023-09-29 17:58:33 +0300

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,12 @@
+let navBar = document.getElementById("navbar")
+let openn = document.getElementById("open")
+let closee = document.getElementById("close")
+openn.onclick = function (){
+    navBar.style.display = "block"
+}
+closee.onclick = function (){
+    navBar.style.display = "none"
+}
+
+
+console.log("gg")
